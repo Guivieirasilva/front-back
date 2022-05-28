ba76ded55cc88f07a487b704f20cb5b21e10d2dc
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-05-28 19:51:58 -0300

    testando as req

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const PORT = 3000
 const express = require("express")
 const bodyParser = require("body-parser")
 
-
 const app = express()
+
+let posts = [
+    {id: "qualquer",
+    title: "Teste do mural",
+    description: "Descrição teste"
+    },
+]
+
+app.get("/all", (req, res) => {
+    res.json(JSON.stringify(posts))
+})
+
+app.post("/new", (req, res) => {
+
+})
 
 app.listen( PORT, () => {
     console.log("Servidor rodando corretamente", PORT);
