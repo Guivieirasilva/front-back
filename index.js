6dcbaf43974e6e4183bc40cc1f7ea1a79b350063
Guilherme Vieira Silva <guivieirasilva02@gmail.com>
2022-05-28 20:04:19 -0300

    implementação o novo post

--- index.js
+++ index.js
@@ -15,10 +15,20 @@ app.get("/all", (req, res) => {
     res.json(JSON.stringify(posts))
 })
 
-app.post("/new", (req, res) => {
+app.post("/new", bodyParser.json(), (req, res) => {
+    let id = generateID()
+    let title = req.body.title
+    let description = req.body.description
 
+    posts.push({id, title, description})
+    
+    res.send("Post adicionado")
 })
 
 app.listen( PORT, () => {
     console.log("Servidor rodando corretamente", PORT);
 })
+
+function generateID(){
+    return Math.random().toString(36).substring(2, 9)
+}
